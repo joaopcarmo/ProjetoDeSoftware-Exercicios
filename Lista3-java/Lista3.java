c628d2c6011a2f179ae1f21881f17d0e53e4ce61
João Pedro Carmo <142856277+joaopcarmo@users.noreply.github.com>
2025-02-25 02:34:01 -0300

    lista3

---
+++ Lista3-java/Lista3.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Lista3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        exercicio1(sc);
+        sc.close();
+    }
+
+    public static void exercicio1(Scanner sc) {
+        int alunosMediaMaiorQue90 = 0, alunosReprovados = 0, numAlunos = 0;
+        int maiorNota = Integer.MIN_VALUE, menorNota = Integer.MAX_VALUE;
+        int nota, totalFaltas;
+        double somaNotas = 0;
+
+        System.out.println("Digite a nota final do aluno (0 a 100) ou um valor negativo para encerrar:");
+        nota = sc.nextInt();
+
+        while (nota >= 0) { // O loop continua enquanto a nota for >= 0
+            System.out.println("Digite o total de faltas:");
+            totalFaltas = sc.nextInt();
+
+            // Contabiliza o número de alunos
+            numAlunos++;
+            somaNotas += nota;
+
+            // Atualiza maior e menor nota
+            if (nota > maiorNota) {
+                maiorNota = nota;
+            }
+            if (nota < menorNota) {
+                menorNota = nota;
+            }
+
+            if (nota >= 90) {
+                alunosMediaMaiorQue90++;
+            }
+
+            if (nota < 70 || totalFaltas >= 20) {
+                alunosReprovados++;
+            }
+
+            System.out.println("Digite a nota final do aluno (0 a 100) ou um valor negativo para encerrar:");
+            nota = sc.nextInt();
+        }
+
+        double media = numAlunos > 0 ? somaNotas / numAlunos : 0;
+
+        System.out.println("Quantidade de alunos que tiveram nota final maior ou igual a 90: " + alunosMediaMaiorQue90);
+        System.out.println("Quantidade de alunos reprovados: " + alunosReprovados);
+        System.out.println("Maior nota: " + (numAlunos > 0 ? maiorNota : "Nenhuma nota cadastrada"));
+        System.out.println("Menor nota: " + (numAlunos > 0 ? menorNota : "Nenhuma nota cadastrada"));
+        System.out.println("Média de notas da turma: " + media);
+    }
+}
